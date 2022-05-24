e8398df0219a58d22cccaba88b5aca4cd495a82e
wrj <846741270@qq.com>
2022-05-24 17:57:37 +0800

    配置了后端api

--- src/api/request.js
+++ src/api/request.js
@@ -1,16 +1,53 @@
 import axios from 'axios'
+import vuex from '../store/index'
 
+// 此项目所有请求均为get请求
 export function request(url, params) {
     const instance = axios.create({
-        baseURL: 'http://www.baidu.com',
-        timeout: 5000,
+        baseURL: '/api',
+        timeout: 3000,
+        // 过期时间 30s
+        withCredentials: true,
+    });
+    // 配置axios拦截器
+    // request拦截
+    instance.interceptors.request.use(data=>{
+        return data;
+    }, error => console.log(error))
+
+    //response拦截
+    instance.interceptors.response.use(data=> {
+        return data
+    }, error => {
+        console.log(error);
+        if(error.response.data.msg === '需要登录') {
+            vuex.state.isLogin = false;
+        } else{
+            console.log(error.data.msg);
+        }
     })
+    instance.defaults.withCredentials = true;
     if (params) {
         params = {
             params: params
         }
-        return instance.get(url, params);
-    }else {
+        return instance.get(url, params)
+    } else {
         return instance.get(url);
     }
 }
+
+// export function request(url, params) {
+//     const instance = axios.create({
+//         baseURL: 'http://www.baidu.com',
+//         timeout: 5000,
+//     })
+//     if (params) {
+//         params = {
+//             params: params
+//         }
+//         return instance.get(url, params);
+//     }else {
+//         return instance.get(url);
+//     }
+// }
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@
 //   transpileDependencies: true,
 //   lintOnSave: false
 // })
+// 配置后台服务
+const serverURL = 'https://netease-cloud-music-lp4woj961-wangrujian0321.vercel.app'
 module.exports = {
   configureWebpack: {
     resolve: {
@@ -16,5 +18,49 @@ module.exports = {
       }
     },
     // lintOnSave: false
+  },
+  devServer: {
+    proxy: {
+      '/api':{
+        target: serverURL,
+        changeOrigin: true,
+        ws: true,
+        pathRewrite:{
+          '^/api': ''
+        },
+        'm7': {
+          target: 'http://m7.music.126.net',
+          changeOrigin: true,
+          ws: true,
+          pathRewrite: {
+            '^/m7': ''
+          }
+        },
+        'm701': {
+          target: 'http://m701.music.126.net',
+          changeOrigin: true,
+          ws: true,
+          pathRewrite: {
+            '^/m701': ''
+          }
+        },
+        'm8': {
+          target: 'http://m8.music.126.net',
+          changeOrigin: true,
+          ws: true,
+          pathRewrite: {
+            '^/m8': ''
+          }
+        },
+        'm801': {
+          target: 'http://m801.music.126.net',
+          changeOrigin: true,
+          ws: true,
+          pathRewrite: {
+            '^/m801': ''
+          }
+        }
+      }
+    }
   }
 }
